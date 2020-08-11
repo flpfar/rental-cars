a8142c5edd8a5a7a2e315aebe32b00b55acc4555
Felipe Rosa <flp.far@gmail.com>
2020-08-10 23:55:28 -0300

    Admin register car category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :car_categories, only: [:index, :show]
+  resources :car_categories, only: [:index, :show, :new, :create]
   resources :subsidiaries, only: [:index, :show]
 end
