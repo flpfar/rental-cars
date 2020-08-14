937508ca6fbc006bb3ff28987eb53267bdad159e
Felipe Rosa <flp.far@gmail.com>
2020-08-14 08:45:26 -0300

    WIP: Admin edit car category

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -2,18 +2,34 @@ class CarCategoriesController < ApplicationController
   def index
     @car_categories = CarCategory.all
   end
+
   def show
     @car_category = CarCategory.find(params[:id])
   end
+
   def new
     @car_category = CarCategory.new
   end
+
   def create
     @car_category = CarCategory.new(car_category_params)
     if @car_category.save
       redirect_to @car_category
     else
       render :new
+    end
+  end
+
+  def edit
+    @car_category = CarCategory.find(params[:id])
+  end
+
+  def update
+    @car_category = CarCategory.find(params[:id])
+    if @car_category.update(car_category_params)
+      redirect_to @car_category
+    else
+      render :edit
     end
   end
 
