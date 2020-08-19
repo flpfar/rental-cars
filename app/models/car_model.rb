5bd51be5e011976353eb4549088f76a66acaf2d8
Felipe Rosa <flp.far@gmail.com>
2020-08-18 23:14:40 -0300

    Admin register car model

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -1,3 +1,5 @@
 class CarModel < ApplicationRecord
   belongs_to :car_category
+
+  validates :name, :year, :motorization, :manufacturer, :fuel_type, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :car_categories
   resources :subsidiaries, only: [:index, :show, :new, :create]
-  resources :car_models, only: [:index, :show]
+  resources :car_models, only: [:index, :show, :new, :create]
 end
