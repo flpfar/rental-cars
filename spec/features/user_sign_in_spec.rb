8b8a9a70dafebf453cdb93222c9a373825e70165
Felipe Rosa <flp.far@gmail.com>
2020-08-19 23:12:20 -0300

    User sign in

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'User sign in' do
+  scenario 'from home page' do
+    visit root_path
+
+    expect(page).to have_link('Entrar')
+  end
+
+  scenario 'successfully' do
+    User.create!(name: 'Felipe', email: 'flp.far@hotmail.com', password: '12345678')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: 'flp.far@hotmail.com'
+    fill_in 'Senha', with: '12345678'
+    click_on 'Entrar'
+
+    expect(page).to have_content('Felipe')
+    expect(page).to have_content('Login efetuado com sucesso')
+    expect(page).to have_link('Sair')
+    expect(page).not_to have_link('Entrar')
+  end
+
+  scenario 'and sign out' do
+    User.create!(name: 'Felipe', email: 'flp.far@hotmail.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'flp.far@hotmail.com'
+    fill_in 'Senha', with: '12345678'
+    click_on 'Entrar'
+    click_on 'Sair'
+
+    expect(page).not_to have_content('Felipe')
+    expect(page).not_to have_content('Login efetuado com sucesso')
+    expect(page).not_to have_link('Sair')
+    expect(page).to have_content('Logout efetuado com sucesso')
+    expect(page).to have_link('Entrar')
+  end
+end
+
